6119ab028370cd938c495ff0f2993a02e49f1580
Shawna Summers <145227217+shawnasummers@users.noreply.github.com>
2023-10-08 08:50:41 -0400

    Add files via upload
    Uploading a new project - Pac Man Horizontal Chomper

---
+++ Portfolio/FrontEnd/html-css-js/pacManHorizontalChomper/pacman.js
@@ -0,0 +1,87 @@
+/*// pos is the PacMan image position variable- it is set to 0 initially
+var pos = 0;
+//pageWidth is the width of the webpage. This is later used to calculate when Pac-Man needs to turn around. 
+let pageWidth = 700; //window.innerWidth;
+//This array contains all the PacMan movement images
+const pacArray = [
+  ['./images/PacMan1.png', './images/PacMan2.png'],
+  ['./images/PacMan3.png', './images/PacMan4.png'],
+];
+
+// this variable defines what direction should PacMan go into:
+// 0 = left to right
+// 1 = right to left (reverse)
+var direction = 0;
+
+// This variable helps determine which PacMan image should be displayed. It flips between values 0 and 1
+var focus = 0;
+
+// This function is called on mouse click. Every time it is called, it updates the PacMan image, position and direction on the screen.
+function Run() {
+  let img = document.getElementById('PacMan');
+  let imgWidth = img.width;
+  focus = (focus + 1) % 2;
+  direction = checkPageBounds(direction, imgWidth, pos, pageWidth);
+  img.src = pacArray[direction][focus];
+  if (direction) {
+    pos -= 20;
+    img.style.left = pos + 'px';
+  } else {
+    pos += 20;
+    img.style.left = pos + 'px';
+  }
+}
+setInterval(Run, 300);
+
+// This function determines the direction of PacMan based on 
+//screen edge detection. 
+function checkPageBounds(direction, imgWidth, pos, pageWidth) {
+  //
+  // TODO: Complete this to reverse direction upon hitting 
+  //screen edge
+  // 
+  //Hits the right side of screen....turn around to the left
+      if(pos+imgWidth == pageWidth)  return 1;
+  //Hits the left side of screen.....turn around to the right
+      else if (pos == 0) return 0;
+  //Keep moving in the current direction    
+      else return direction;
+}
+
+//Please do not change
+module.exports = checkPageBounds;
+*/
+
+var pos = 0;
+let pageWidth = window.innerWidth;
+const pacArray = [
+  ["./images/PacMan1.png", "./images/PacMan2.png"],
+  ["./images/PacMan3.png", "./images/PacMan4.png"],
+];
+var direction = 0;
+var focus = 0;
+
+function Run() {
+  let img = document.getElementById("PacMan");
+  let imgWidth = img.width;
+  focus = (focus + 1) % 2;
+  direction = checkPageBounds(direction, imgWidth, pos, pageWidth);
+  img.src = pacArray[direction][focus];
+  if (direction) {
+    pos -= 20;
+    img.style.left = pos + "px";
+  } else {
+    pos += 20;
+    img.style.left = pos + "px";
+  }
+}
+setInterval(Run, 300);
+
+function checkPageBounds(direction, imgWidth, pos, pageWidth) {
+  if (direction == 0 && pos + imgWidth > pageWidth) direction = 1;
+  if (direction == 1 && pos < 0) direction = 0;
+
+  return direction;
+}
+
+module.exports = checkPageBounds;
