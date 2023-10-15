40ac15429cfb82bfc355299e0d019bab201c8be4
Shawna Summers <145227217+shawnasummers@users.noreply.github.com>
2023-10-15 10:59:42 -0400

    Add files via upload
    Adding new project - Arrow Functions

---
+++ Portfolio/FrontEnd/html-css-js/arrowFunctions/convertFunctionsFatArrows.js
@@ -0,0 +1,32 @@
+/*function addFive(num) {
+    return num + 5;
+  }
+ */
+ // TODO: Convert addFive() to arrow function
+  
+  //let addFive get num where num equals num + 5
+  let addFive = num => num+5;
+  
+/*function divideBy(x, y) {
+    return x / y;
+  }
+  */
+  // TODO: Convert divideBy() to arrow function
+  //let divideBy get x, y where x, y equals x/y
+  let divideBy = x,y => x/y;
+  
+/*function logToConsole() {
+    let msg = "MIT Coding Certificate";
+    console.log(msg);
+  }
+  */
+  // TODO: Convert logToConsole() to arrow function
+  //let msg get msg where msg equals console.log(msg)
+  let logToConsole = msg => {
+    msg = "MIT Coding Certificate";
+    console.log(msg);
+  }
+
+  //addFive(10);
+  //divideBy(20,2);
+  //logToConsole(msg);
